456ec1d191b9f91e93fbb700b3a7dda5b6efedaf
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-03-16 16:31:01 +0100

    tested Conditional Rendering

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const app = new Vue({
     el: '#app',
     data: {
       message: 'Hello Vue!',
-      immagine: 'https://as01.epimg.net/us/imagenes/2021/08/27/futbol/1630090262_737972_1630091087_noticia_normal_recorte1.jpg'
+      immagine: 'https://as01.epimg.net/us/imagenes/2021/08/27/futbol/1630090262_737972_1630091087_noticia_normal_recorte1.jpg',
+      color: ""
     },
 
     methods: {
@@ -13,6 +14,17 @@ const app = new Vue({
         },
         pulisciImmagine: function () {
             this.immagine = "";
-          }
+          
+        },
+        rosso: function () {
+            this.color = "rosso";
+            console.log(this.color);
+        
+        } ,
+        blu: function () {
+            this.color = "blu";
+            console.log(this.color);
+        }
     }
+
   });
